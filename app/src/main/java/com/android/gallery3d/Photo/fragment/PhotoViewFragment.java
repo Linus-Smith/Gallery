c66cd7d9705016809b1915eabef6dc2cd165db74
linus.yang <linus.yang@coolpad.com>
2017-05-16 11:55:24 +0800

    add

---
+++ app/src/main/java/com/android/gallery3d/Photo/fragment/PhotoViewFragment.java
@@ -0,0 +1,16 @@
+package com.android.gallery3d.Photo.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Linus on 2017/5/16.
+ */
+
+public class PhotoViewFragment  extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
